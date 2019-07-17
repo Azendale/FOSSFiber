63a34240be6f31d0b0ab21c46e5a53ffc2f11aef
Erik B. Andersen <erik.b.andersen@gmail.com>
2019-07-16 23:38:14 -0700

    First basic steps to getting leaflet maps to work in admin.

--- FOSSfiberweb/fiberrecords/admin.py
+++ FOSSfiberweb/fiberrecords/admin.py
@@ -1,3 +1,6 @@
+from leaflet.admin import LeafletGeoAdmin
 from django.contrib import admin
+from . import importedModels
 
 # Register your models here.
+admin.site.register(importedModels.UtilityPole, LeafletGeoAdmin)
