08ed39feb173405b775cc26425d0122bcd4d0895
Erik B. Andersen <erik.b.andersen@gmail.com>
2019-08-14 00:41:40 -0700

    Adjust this branch to hande importing DB models better. Will need to undo this commit when we merge with master.

--- FOSSfiberweb/fiberrecords/admin.py
+++ FOSSfiberweb/fiberrecords/admin.py
@@ -1,6 +1,4 @@
 from leaflet.admin import LeafletGeoAdmin
 from django.contrib import admin
-from . import importedModels
 
 # Register your models here.
-admin.site.register(importedModels.UtilityPole, LeafletGeoAdmin)
