afabec0f6d761de2a6eb40413701699f1b3e02b0
Erik B. Andersen <erik.b.andersen@gmail.com>
2019-12-26 19:09:59 -0800

    Clean up old file that was split up.

--- fossfiber_create_set_permissions.sql
+++
@@ -1,6 +0,0 @@
-create database fossfiber;
-create extention postgis;
-
-GRANT ALL PRIVILEGES ON ALL SEQUENCES in schema public to fossfiber1;
-GRANT ALL PRIVILEGES ON ALL FUNCTIONS in schema public to fossfiber1;
-GRANT ALL PRIVILEGES ON ALL TABLES in schema public to fossfiber1;
